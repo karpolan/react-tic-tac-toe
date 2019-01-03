4cdaa2e03cdea25f2ca02919449c2ffe8a98d129
Anton Karpenko <karpolan@karpolan.com>
2019-01-03 16:15:03 +0200

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import Game from './containers/Game/Game';
+import React, { Component } from "react";
+import "./App.css";
+import Game from "./containers/Game/Game";
 
 class App extends Component {
   render() {
@@ -10,6 +10,28 @@ class App extends Component {
           <h1>Tic Tac Toe game on React JS</h1>
         </header>
         <Game />
+        <footer>
+          <p>
+            Source codes are available on GitHub:{" "}
+            <a
+              rel="author noopener noreferrer"
+              target="_blank"
+              href="https://github.com/karpolan/react-tic-tac-toe"
+            >
+              React JS demo - Tic Tac Toe game
+            </a>
+          </p>
+          <p>
+            Copyright &copy;{" "}
+            <a
+              rel="author noopener noreferrer"
+              target="_blank"
+              href="https://karpolan.com"
+            >
+              KARPOLAN
+            </a>
+          </p>
+        </footer>
       </div>
     );
   }
